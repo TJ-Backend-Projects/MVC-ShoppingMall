5dce4fd42e2cb45762cbdb6e6e614958acf8ef37
LeeMinkyu <kfivxz0@gmail.com>
2024-05-14 22:47:13 +0900

    feat : 유저 회원가입 기능 구현
    - 로그인 페이지, 회원가입 페이지를 구성
    - 유저 회원가입 기능 구현
    - 회원가입 시 비밀번호 일치 여부 확인후 결과표시
    - 비밀번호, 비밀번호 확인 불일치 시 알림창 표

--- shoppingmall/src/main/java/mvc/controller/UserController.java
+++ shoppingmall/src/main/java/mvc/controller/UserController.java
@@ -1,41 +1,70 @@
 package mvc.controller;
 
 import java.io.IOException;
+import java.sql.Date;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
 
-/**
- * Servlet implementation class UserController
- */
-@WebServlet("/UserController")
+import mvc.dao.UserDAO;
+import mvc.dto.UserDTO;
+
+@WebServlet("*.do")
 public class UserController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UserController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doProcess(request,response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		doProcess(request,response);
 	}
 
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doProcess");
+		request.setCharacterEncoding("utf-8"); // 한글처리
+	
+		String uri = request.getRequestURI();
+		String action = uri.substring(uri.lastIndexOf("/"));
+		System.out.println(uri);
+		
+		if(action.equals("/join.do")) {
+			String path = request.getContextPath() + "/user/join.jsp";
+			response.sendRedirect(path);	
+		} else if(action.equals("/login.do")) {
+			String path = request.getContextPath() + "/user/login.jsp";
+			response.sendRedirect(path);
+		} else if(action.equals("/joinProc.do")) {
+			request.setCharacterEncoding("utf-8");
+			 String userId = request.getParameter("userId");
+			 String password = request.getParameter("password");			 
+			 String userName = request.getParameter("userName");
+			 String userAddress = request.getParameter("userAddress");
+			 String email = request.getParameter("email");
+			 Date registrationDate = new Date(System.currentTimeMillis());
+			 String tel = request.getParameter("tel");
+			 Integer age = null; // 초기화
+			 String ageParameter = request.getParameter("age");
+			 if (ageParameter != null && !ageParameter.isEmpty()) {
+			     try {
+			         age = Integer.valueOf(ageParameter); // 문자열을 Integer로 변환
+			     } catch (NumberFormatException e) {
+			         e.printStackTrace();
+			     }
+			 }
+			 
+             // DB 처리
+			 UserDAO dao = new UserDAO();
+			 UserDTO dto = new UserDTO(userId, password, userName, userAddress, email, registrationDate, tel, age);
+			 dao.insertUser(dto);
+			
+//			 로그인 페이지로 이동
+			String path = request.getContextPath() +"/user/login.do";
+			response.sendRedirect(path);
+		}
+	}
+	
 }
---
+++ shoppingmall/src/main/webapp/user/login.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>login.jsp</title>
+</head>
+<body>
+	<center>
+		<h2>회원로그인</h2>
+		<form action="loginProc.do" method="post">
+		<input type="text" name="userId">
+		<input type="password" name="password">
+		<input type="submit" value="로그인">
+		</form>
+		<a href="join.do">회원가입</a>
+	</center>
+</body>
+</html>
